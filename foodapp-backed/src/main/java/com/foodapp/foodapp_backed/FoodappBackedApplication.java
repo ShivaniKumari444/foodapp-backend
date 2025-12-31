81266fdfb81bd9b6d780c9296e7d8d6e25ea6793
Shivani Kumari <51056985+ShivaniKumari444@users.noreply.github.com>
2025-12-31 17:10:05 +0530

    Add files via upload

---
+++ foodapp-backed/src/main/java/com/foodapp/foodapp_backed/FoodappBackedApplication.java
@@ -0,0 +1,13 @@
+package com.foodapp.foodapp_backed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodappBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodappBackedApplication.class, args);
+	}
+
+}
---
+++ foodapp-backed/src/test/java/com/foodapp/foodapp_backed/FoodappBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodapp.foodapp_backed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodappBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
